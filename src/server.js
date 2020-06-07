fd57ba1cc9b1eb37500a1e18c89e1c61e0709a07
MaduSouza26 <duardagregorio@gmail.com>
2020-06-07 15:23:10 -0300

    Aula 4 - nlw

--- src/server.js
+++ src/server.js
@@ -1,17 +1,32 @@
-const express = require ("express")
-const server = express ()
+const express = require("express")
+const server = express()
 
-// configurar pasta pública
-server.use (express.static ("public"))
 
-// configurar caminhos da minha aplicação
-// página inicial
-// req: Requisição (pedido)
-// res: Resposta
-server.get ("/", (req, res) => {
-    res.sendFile (__dirname + "/views/index.html")
+// configurar pasta publica
+server.use(express.static("public"))
+
+// utilizando template engine
+const nunjucks = require ("nunjucks")
+nunjucks.configure ("src/views", {
+    express: server,
+    noCache: true
 })
 
 
-// ligar o servidor
+// configurar caminhos da minha aplicação
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render ("index.html", { title: "Um título"})
+})
+
+server.get("/views/create-point.html", (req, res) => {
+    return res.render ("create-point.html")
+})
+
+server.get("/search-results.html", (req, res) => {
+    return res.render ("search-results.html")
+})
+
 server.listen (3000)
